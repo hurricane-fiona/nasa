396290862ee719217cf8a3e59133d6c596a6cfb7
Anmol Vatsa <vibhor.vatsa@gmail.com>
2022-09-29 22:49:44 -0300

    adding retriable logic

--- article_downloder/NTRSClient.py
+++ article_downloder/NTRSClient.py
@@ -2,10 +2,43 @@ import requests
 import json
 
 
-collected_data = []
+
+def do_search():
+    collected_data = []
+    total_download_count = 0
+    total_text_download_count = 0
+    for page_idx in range(START_PAGE, MAX_PAGE_NUMBER, MAX_PAGE_SIZE):
+        req_body = search_body
+        req_body["page"]["from"] = page_idx
+        citation_count, download_count, text_download_counts = do_search_request(req_body)
+        total_text_download_count += text_download_counts
+        total_download_count += download_count
+        if total_text_download_count>2000:
+            break
+        #    if citation_count < MAX_PAGE_SIZE:
+        #        break
+    with open('collected_data.json', 'w', encoding='utf-8') as f:
+        json.dump(collected_data, f, ensure_ascii=False, indent=4)
+
+    print("Total downloaded count = "+str(total_download_count))
+    print("Total text downloaded count = "+str(total_text_download_count))
+
+def retriable_request(request):
+    max_attempt = 2
+    retry_attempt = 0
+    resp = 200
+    while (not resp.status_code == 500) and retry_attempt <= max_attempt:
+        resp = request()
+        retry_attempt+=1
+        limit_remaining = resp.headers["x-ratelimit-remaining"]
+        if limit_remaining <= 0:
+            print("rate Limited till "+ str(resp.headers["x-ratelimit-reset"]))
+            break
+    return resp
 
 def do_search_request(search_body):
-    resp = requests.post(SEARCH_URL, data=json.dumps(search_body), headers = headers)
+    request = lambda : requests.post(SEARCH_URL, data=json.dumps(search_body), headers = headers)
+    resp = retriable_request(request)
     if(resp.status_code == requests.codes.ok):
         search_resp = resp.json()
         citations = search_resp["results"]
@@ -33,9 +66,11 @@ def do_search_request(search_body):
                             text_download_counts += 1
         print("Downloads available for "+str(download_count))
         print("Text Downloads available for "+str(text_download_counts))
+        return len(citations), download_count, text_download_counts
     else:
         print(resp.status_code)
-    return len(citations), download_count, text_download_counts
+        print(resp.text)
+    return
 
 
 BASE_NTRS_URL = "https://ntrs.nasa.gov/api"
@@ -69,24 +104,10 @@ search_body = {
 # }
 headers = {"Content-Type": "application/json"}
 ids_seen = set()
-total_download_count = 0
-total_text_download_count = 0
-for page_idx in range(START_PAGE, MAX_PAGE_NUMBER, MAX_PAGE_SIZE):
-    req_body = search_body
-    req_body["page"]["from"] = page_idx
-    citation_count, download_count, text_download_counts = do_search_request(req_body)
-    total_text_download_count += text_download_counts
-    total_download_count += download_count
-    if total_text_download_count>2000:
-        break
- #    if citation_count < MAX_PAGE_SIZE:
- #        break
 
-print("Total downloaded count = "+str(total_download_count))
-print("Total text downloaded count = "+str(total_text_download_count))
+do_search()
 
-import json
-with open('collected_data.json', 'w', encoding='utf-8') as f:
-    json.dump(collected_data, f, ensure_ascii=False, indent=4)
+
+
 
 
