597a54e552094203931b966cf3d26d8a26f20b16
DeviAyyagari <DeviAyyagari@T9417.WPA.Dal.Ca>
2022-10-01 12:45:21 -0300

    add link to pdf and link to txt to dummy json object

--- data/dummy/data_handler.py
+++ data/dummy/data_handler.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import json
 
+BASE_NTRS_URL = "https://ntrs.nasa.gov"
 
 def get_dummy_data():
     # Opening JSON file
@@ -13,6 +14,8 @@ def get_dummy_data():
         'abstract': [],
         'id': [],
         'keywords':[],
+        'text_link': [],
+        'pdf_link': []
     }
     data = json.load(f)
 
@@ -23,13 +26,16 @@ def get_dummy_data():
         abstract = data_item['abstract'] if 'abstract' in data_item else ''
         id_ = data_item['id'] if 'id' in data_item else ''
         keywords = ';'.join(data_item['subjectCategories']) if 'subjectCategories' in data_item else ''
+        text_link = BASE_NTRS_URL + data_item['downloads'][0]['links']['fulltext'] if 'downloads' in data_item and 'fulltext' in data_item['downloads'][0]['links'] else ''
+        pdf_link = BASE_NTRS_URL + data_item['downloads'][0]['links']['pdf'] if 'downloads' in data_item and 'pdf' in data_item['downloads'][0]['links'] else ''
 
         d['title'].append(title)
         d['abstract'].append(abstract)
         d['id'].append(id_)
         d['keywords'].append(keywords)
-        # link to the pdf
-        # link to the full text
+        d['text_link'].append(text_link)
+        d['pdf_link'].append(pdf_link)
+
 
     # Closing file
     f.close()
