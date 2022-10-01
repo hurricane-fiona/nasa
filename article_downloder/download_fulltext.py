0b540776f8c887493fc0cc2aa51a6fa0f176c75a
DeviAyyagari <DeviAyyagari@T9417.WPA.Dal.Ca>
2022-10-01 10:32:35 -0300

    added functionality to download a txt file for each json file

--- article_downloder/download_fulltext.py
+++ article_downloder/download_fulltext.py
@@ -1,5 +1,7 @@
 import json
 import NTRSClient
+import glob
+import os
 
 def read_json(metadata_file):
     with open(metadata_file, 'r') as f:
@@ -22,5 +24,21 @@ def download_and_save(url):
     content = NTRSClient.download(url)
     open(url.split("/")[-1], "wb").write(content)
 
+def download_from_dir():
+    json_files = glob.glob("*.json")
+    txt_files = glob.glob("*.txt")
+    to_download = list(set([each.replace(".json", "") for each in json_files]) - set([each.replace(".txt", "") for each in json_files]))
+    to_download = [each+".json" for each in to_download]
+    print("{} files exist. Downloading {}".format(len(txt_files), len(to_download)))
+
+    print(to_download)
+    for index, metadata_file in enumerate(to_download):
+        print("Downloading {} of {} files".format(index, len(json_files)))
+        for each in metadata[index]["downloads"]:
+            URL = each['links']['fulltext']
+            download_and_save(URL)
+
+
 if __name__ == "__main__":
-    download_n_full_textfiles('collected_data.json', 100)
+    # download_n_full_textfiles('collected_data.json', 100)
+    download_from_dir()
